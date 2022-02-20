8c63d7862b9b47492fd2017307db091cb3d42bb3
Electronica-dev <srsamartha@gmail.com>
2022-02-20 14:18:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,129 @@
+const { PythonShell } = require('python-shell')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { fs, vol } = require('memfs')
+const https = require('https')
+const api_key = process.env.REACT_APP_API_KEY
+
+app.use(cors())
+
+const download = (url, dest, cb) => {
+  var file = vol.createWriteStream(dest);
+
+  var req = https.get(url, function(res) {
+    res.pipe(file);
+    file.on('finish', function() {
+      file.close(cb(dest));  // close() is async, call cb after close completes.
+    });
+    }).on('error', function(err) { // Handle errors
+      fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    });
+};
+
+const getHistory = (interval, symbol, cb) => {
+
+  const path = `/v8/finance/spark?interval=${interval}&range=max&symbols=${symbol}`
+
+  let body = ''
+
+  const options = {
+    host: 'yfapi.net',
+    path: path,
+    headers: {
+      'Accept': 'application/json',
+      'x-api-key': `${api_key}`
+    }
+  };
+  const req = https.get(options, function (res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      body = body.concat(chunk)
+    }).on('end', () => {
+      cb(body)
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+    });
+  })
+}
+
+app.get('/api/stock/key', (request, response) => {
+  console.log(api_key);
+  response.send(
+    `<p>${api_key}</p>`
+  )
+})
+
+app.get('/api/stock/bse', (request, response) => {
+
+  let bseFinalJson = []
+
+  const getBseStocks = (dest) => {
+    const re = /(?<=\d{6},).*(?=,.*,Active)/g;
+    const bseJson = vol.toJSON()
+    const bseStocks = bseJson[dest].match(re)
+    bseStocks.forEach(x => {
+        if(x[0] === '"')
+          bseFinalJson = bseFinalJson.concat({label: x.split(",")[2], value: x.split(",")[0].slice(1,), exchange: 'BSE'})
+        else
+          bseFinalJson = bseFinalJson.concat({label: x.split(",")[1], value: x.split(",")[0], exchange: 'BSE'})
+      }
+    )
+    response.json(bseFinalJson)
+    response.end()
+  }
+  download('https://api.bseindia.com/BseIndiaAPI/api/LitsOfScripCSVDownload/w?segment=Equity&status=Active', '/bse.csv', getBseStocks)
+})
+
+app.get('/api/stock/nse', (request, response) => {
+
+  let nseFinalJson = []
+
+  const getNseStocks = (dest) => {
+    const re = /(?<=\n).*(?=,[A-Z]{2},)/g;
+    const nseJson = vol.toJSON()
+    const nseStocks = nseJson[dest].match(re)
+    nseStocks.forEach(x => {
+        nseFinalJson = nseFinalJson.concat( {label: x.split(",")[0], value: x.split(",")[1], exchange: 'NSE'} )
+      }
+    )
+    response.json(nseFinalJson)
+    response.end()
+  }
+  download('https://archives.nseindia.com/content/equities/EQUITY_L.csv', '/nse.csv', getNseStocks)
+})
+
+app.get('/api/stock/predict/:symbol-:interval-:price-:no_of_candles', (request, response) => {
+
+  const symbol = request.params.symbol
+  const interval = request.params.interval
+  const price = parseInt(request.params.price)
+  const candles = parseInt(request.params.no_of_candles)
+
+  const predict = (body) => {
+
+    const close = {"close": JSON.parse(body)[symbol].close}
+
+    let options = {
+      mode: 'json',
+      pythonPath: 'python',
+      pythonOptions: ['-u'], // get print results in real-time
+      args: [JSON.stringify(close), price, candles]
+    };
+
+    PythonShell.run('prediction_model.py', options, function (err, results) {
+      if (err) throw err;
+      response.send(results)
+      response.end()
+    });
+
+  }
+
+  getHistory(interval, symbol, predict)
+
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ prediction_model.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 13 12:07:58 2022
+
+@author: Arun Kumar
+"""
+import pandas as pd
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+import datetime as dt
+# import pandas_ta
+import matplotlib.pyplot as plt
+import sys
+import json
+
+# get parameters from command line
+json_file = sys.argv[1]
+stock_price = int(sys.argv[2])
+no_of_days = int(sys.argv[3])
+
+# extracting data
+df = pd.read_json(json_file)
+
+#keeping only adjusted closing price
+df = df[['close']]
+
+#finding moving avrage for n number of days
+df['EMA'] = df['close'].ewm(span = no_of_days).mean()
+df = df.iloc[10:]
+
+#splitting data
+x_train, x_test, y_train, y_test = train_test_split(df[['close']], df.EMA, train_size = 0.7)
+
+#training model
+price = linear_model.LinearRegression()
+price.fit(x_train, y_train)
+
+#finding accuracy of the model
+predicted_price = float('{:.3f}'.format(price.predict([[stock_price]])[0]))
+
+score = price.score(x_test,y_test)
+predicted_accuracy = float('{:.1f}'.format(score*100))
+
+print(json.dumps([predicted_price, predicted_accuracy]))
